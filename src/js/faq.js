a56637007911bdbfcf9cdcbfe597851e30011848
SychTim <103521623+SychTim@users.noreply.github.com>
2024-07-27 11:48:10 +0200

    fix main js

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,9 +2,11 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import '../css/faq.css';
 
-new Accordion('.faq-list', {
-  elementClass: 'faq-item',
-  triggerClass: 'faq-list-btn',
-  panelClass: 'faq-list-panel',
-  showMultiple: true,
-});
+export function faq() {
+  new Accordion('.faq-list', {
+    elementClass: 'faq-item',
+    triggerClass: 'faq-list-btn',
+    panelClass: 'faq-list-panel',
+    showMultiple: true,
+  });
+}
