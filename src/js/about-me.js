aad3af3b99a7d2ab3ed1f14868c1ee56dbd31be8
Nataliia Horokhovatska <nataliia.horokhovatska@gmail.com>
2024-07-28 11:29:52 +0300

    update aboutme js

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,65 +3,55 @@ import Accordion from 'accordion-js';
 import Swiper from 'swiper';
 import '../../node_modules/swiper/swiper.css';
 
-new Accordion('.accordion-container-first', {
-  duration: 600,
-  showMultiple: true,
-  openOnInit: [0],
-});
-
-const accordionsList = document.querySelectorAll('.aboutme-accordion-item');
-accordionsList.forEach(function (item) {
-  const title = item.querySelector('.aboutme-title-accordion');
-  const btnDown = item.querySelector('.ac-trigger');
-  btnDown.addEventListener('mouseenter', function () {
-    title.classList.add('hovered');
+export function aboutmeaccordion() {
+  new Accordion('.accordion-container-first', {
+    duration: 600,
+    showMultiple: true,
+    openOnInit: [0],
   });
+}
+export function aboutmeswipper() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.aboutme-skills-circle');
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
+    slides.forEach((slide, index) => {
+      const clone = slide.cloneNode(true);
+      swiperWrapper.appendChild(clone);
+    });
 
-  btnDown.addEventListener('mouseleave', function () {
-    title.classList.remove('hovered');
-  });
-});
+    const swiper = new Swiper('.aboutme-skills-swiper', {
+      speed: 400,
+      spaceBetween: 0,
+      loop: true,
+      setWrapperSize: true,
+      breakpoints: {
+        1440: {
+          slidesPerView: 6,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        320: {
+          slidesPerView: 2,
+        },
+      },
+      on: {
+        init: function () {
+          document.querySelector('.swiper-slide').style.backgroundColor =
+            '#ed3b44';
+        },
+        slideChange: function () {
+          const previousSlide = this.slides[this.previousIndex];
+          const activeSlide = this.slides[this.activeIndex];
+          previousSlide.style.background = '';
+          activeSlide.style.backgroundColor = '#ed3b44';
+        },
+      },
+    });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const slides = document.querySelectorAll('.aboutme-skills-circle');
-  const swiperWrapper = document.querySelector('.swiper-wrapper');
-  slides.forEach((slide, index) => {
-    const clone = slide.cloneNode(true);
-    swiperWrapper.appendChild(clone);
+    const btnNext = document.querySelector('.aboutme-skills-btn');
+    btnNext.addEventListener('click', () => {
+      swiper.slideNext();
+    });
   });
-
-  const swiper = new Swiper('.aboutme-skills-swiper', {
-    speed: 400,
-    spaceBetween: 0,
-    loop: true,
-    setWrapperSize: true,
-    breakpoints: {
-      1440: {
-        slidesPerView: 6,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      320: {
-        slidesPerView: 2,
-      },
-    },
-    on: {
-      init: function () {
-        document.querySelector('.swiper-slide').style.backgroundColor =
-          '#ed3b44';
-      },
-      slideChange: function () {
-        const previousSlide = this.slides[this.previousIndex];
-        const activeSlide = this.slides[this.activeIndex];
-        previousSlide.style.background = '';
-        activeSlide.style.backgroundColor = '#ed3b44';
-      },
-    },
-  });
-
-  const btnNext = document.querySelector('.aboutme-skills-btn');
-  btnNext.addEventListener('click', () => {
-    swiper.slideNext();
-  });
-});
+}
