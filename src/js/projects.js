8b83631be766a17aa72147ecbbe08f957e69bb20
SychTim <103521623+SychTim@users.noreply.github.com>
2024-07-30 13:59:00 +0200

    fixs swiper

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,26 +2,42 @@ import Swiper from 'swiper/bundle';
 // import 'swiper/css/bundle';
 import { Navigation, Keyboard } from 'swiper/modules';
 
+const buttPrev = document.querySelector('.js-prevPr');
+const buttNext = document.querySelector('.js-nextPr');
+
 export function addSwiperProj() {
   const swiperProjects = new Swiper('.swiper-projects', {
     // Optional parameters
     modules: [Navigation, Keyboard],
     speed: 500,
+    spaceBetween: 5,
+    grabCursor: true,
+    allowTouchMove: true,
     direction: 'horizontal',
+    watchOverflow: true,
   
     // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    //   keyboard: {
-    //     enabled: true,
-    //     onlyInViewport: true,
-    //     pageUpDown: true,
-    //   },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      },
   });
 
-swiperProjects();
+  buttNext.addEventListener('click', nextSlide);
+  buttPrev.addEventListener('click', prevSlide);
+
+  function nextSlide() {
+    swiperProjects.slideNext();
+  }
+
+  function prevSlide() {
+    swiperProjects.slidePrev();
+  }
 }
 
 
